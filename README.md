2252af4722332f10fc198cbd94550c965675c9d2
opennota <opennota@gmail.com>
2016-02-01 21:26:06 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+nbgate [![License](http://img.shields.io/:license-gpl3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0.html)
+======
+
+nbgate is a reverse proxy to [notabenoid.org](http://notabenoid.org). It provides access to the site under a single common account while preventing the account from being hijacked.
+
+## Install
+
+    go get github.com/opennota/nbgate
+
+## Run
+
+    nbgate -u username -p password
---
+++ main.go
@@ -0,0 +1,163 @@
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Reverse proxy to notabenoid.org
+package main
+
+import (
+	"errors"
+	"flag"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"regexp"
+)
+
+const (
+	host        = "notabenoid.org"
+	baseURL     = "http://notabenoid.org/"
+	settingsURL = baseURL + "register/settings"
+)
+
+var (
+	user = flag.String("u", "", "Username")
+	pass = flag.String("p", "", "Password")
+	addr = flag.String("http", ":1337", "HTTP service address")
+
+	jar, _ = cookiejar.New(nil)
+	c      = http.Client{Jar: jar}
+
+	rSensitivePath = regexp.MustCompile(`(?i)^/register|^/users/\d+/(edit|delete)`)
+)
+
+func copyHeader(dst, src http.Header) {
+	for k, vs := range src {
+		for _, v := range vs {
+			dst.Add(k, v)
+		}
+	}
+}
+
+func logRequest(r *http.Request) {
+	host, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		host = r.RemoteAddr
+	}
+	log.Println(host, r.Method, r.URL.String(), r.UserAgent())
+}
+
+var removeHeaders = []string{
+	"Cookie",
+	"Set-Cookie",
+	"Connection",
+	"Keep-Alive",
+}
+
+func reverseProxy(w http.ResponseWriter, req *http.Request) {
+	logRequest(req)
+
+	if rSensitivePath.MatchString(req.URL.Path) {
+		http.Error(w, "403 Forbidden", http.StatusForbidden)
+		return
+	}
+
+	outReq := new(http.Request)
+	outReq.Method = req.Method
+	outReq.URL = &url.URL{
+		Scheme:   "http",
+		Host:     host,
+		Path:     req.URL.Path,
+		RawQuery: req.URL.RawQuery,
+	}
+	outReq.Proto = "HTTP/1.1"
+	outReq.ProtoMajor = 1
+	outReq.ProtoMinor = 1
+	outReq.Header = make(http.Header)
+	outReq.Body = req.Body
+	outReq.ContentLength = req.ContentLength
+	outReq.Host = host
+
+	for _, h := range removeHeaders {
+		req.Header.Del(h)
+	}
+	copyHeader(outReq.Header, req.Header)
+	outReq.Header.Set("Host", host)
+	outReq.Header.Set("Referer", baseURL)
+	outReq.Header.Set("Origin", baseURL)
+
+	resp, err := c.Do(outReq)
+	if err != nil {
+		http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+		log.Printf("ERROR: can't contact %s: %v", host, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	for _, h := range removeHeaders {
+		resp.Header.Del(h)
+	}
+	copyHeader(w.Header(), resp.Header)
+	w.WriteHeader(resp.StatusCode)
+
+	_, err = io.Copy(w, resp.Body)
+	if err != nil {
+		log.Printf("ERROR: io.Copy: %v", err)
+	}
+}
+
+func robotsHandler(w http.ResponseWriter, req *http.Request) {
+	logRequest(req)
+	w.Write([]byte("User-agent: *\nDisallow: /\n"))
+}
+
+func login() error {
+	loginForm := url.Values{
+		"login[login]": {*user},
+		"login[pass]":  {*pass},
+	}
+	resp, err := c.PostForm(baseURL, loginForm)
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return errors.New(resp.Status)
+	}
+
+	resp, err = c.Get(settingsURL)
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return errors.New(resp.Status)
+	}
+
+	return nil
+}
+
+func main() {
+	flag.Parse()
+
+	if err := login(); err != nil {
+		log.Fatalf("ERROR: failed to log in: %v. Invalid username or password?", err)
+	}
+
+	http.HandleFunc("/", reverseProxy)
+	http.HandleFunc("/robots.txt", robotsHandler)
+	log.Println("listening on", *addr)
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
